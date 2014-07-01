3bdc1102cff6468b16e1b50c7b167abd21c312f6
Felipe Schmidt <felipefsch@gmail.com>
2014-07-01 15:11:32 -0300

    Adding final topology and controller for presentation

--- final_topo.py
+++ final_topo.py
@@ -32,30 +32,30 @@ class MyTopo( Topo ):
                            9: {4: 2, 6: 3},
                            10: {5: 4, 6: 5}}
 
-    self.ip_to_switch_port = dict({'10.0.0.11': [1,1],
-                                   '10.0.0.12': [2,1],
-                                   '10.0.0.13': [3,1],
-                                   '10.0.0.14': [4,1],
-                                   '10.0.0.15': [5,1],
-                                   '10.0.0.16': [6,1],
-                                   '10.0.0.17': [7,1],
-                                   '10.0.0.18': [8,1],
-                                   '10.0.0.19': [9,1],
-                                   '10.0.0.20': [10,1],
-                                   '10.0.0.21': [10,2],
-                                   '10.0.0.22': [10,3]})
+    self.ip_to_switch_port = dict({'10.0.0.1': [1,1],
+                                   '10.0.0.2': [2,1],
+                                   '10.0.0.3': [3,1],
+                                   '10.0.0.4': [4,1],
+                                   '10.0.0.5': [5,1],
+                                   '10.0.0.6': [6,1],
+                                   '10.0.0.7': [7,1],
+                                   '10.0.0.8': [8,1],
+                                   '10.0.0.9': [9,1],
+                                   '10.0.0.10': [10,1],
+                                   '10.0.0.11': [10,2],
+                                   '10.0.0.12': [10,3]})
 
     # Switch DPID [ip1..ipn]
-    self.switch_ips = dict({1:['10.0.0.11'],
-                            2:['10.0.0.12'],
-                            3:['10.0.0.13'],
-                            4:['10.0.0.14'],
-                            5:['10.0.0.15'],
-                            6:['10.0.0.16'],
-                            7:['10.0.0.17'],
-                            8:['10.0.0.18'],
-                            9:['10.0.0.19'],
-                            10:['10.0.0.20','10.0.0.21','10.0.0.22']})
+    self.switch_ips = dict({1:['10.0.0.1'],
+                            2:['10.0.0.2'],
+                            3:['10.0.0.3'],
+                            4:['10.0.0.4'],
+                            5:['10.0.0.5'],
+                            6:['10.0.0.6'],
+                            7:['10.0.0.7'],
+                            8:['10.0.0.8'],
+                            9:['10.0.0.9'],
+                            10:['10.0.0.10','10.0.0.11','10.0.0.12']})
     """
 
     def __init__( self ):
@@ -65,42 +65,42 @@ class MyTopo( Topo ):
         Topo.__init__( self )
 
         # Add hosts and switches
-        h1 = self.addHost( 'h11' )
-        h2 = self.addHost( 'h12' )
-        h3 = self.addHost( 'h13' )
-        h4 = self.addHost( 'h14' )
-        h5 = self.addHost( 'h15' )
-        h6 = self.addHost( 'h16' )
-        h7 = self.addHost( 'h17' )
-        h8 = self.addHost( 'h18' )
-        h9 = self.addHost( 'h19' )
-        h10 = self.addHost( 'h20' )
-        h11 = self.addHost( 'h21' )
-        h12 = self.addHost( 'h22' )
-        s9 = self.addSwitch( 's1' )
-        s10 = self.addSwitch( 's2' )
-        s11 = self.addSwitch( 's3' )
-        s12 = self.addSwitch( 's4' )
-        s13 = self.addSwitch( 's5' )
-        s13 = self.addSwitch( 's6' )
-        s13 = self.addSwitch( 's7' )
-        s13 = self.addSwitch( 's8' )
-        s13 = self.addSwitch( 's9' )
-        s13 = self.addSwitch( 's10' )
+        h11 = self.addHost( 'h11' )
+        h12 = self.addHost( 'h12' )
+        h13 = self.addHost( 'h13' )
+        h14 = self.addHost( 'h14' )
+        h15 = self.addHost( 'h15' )
+        h16 = self.addHost( 'h16' )
+        h17 = self.addHost( 'h17' )
+        h18 = self.addHost( 'h18' )
+        h19 = self.addHost( 'h19' )
+        h20 = self.addHost( 'h20' )
+        h21 = self.addHost( 'h21' )
+        h22 = self.addHost( 'h22' )
+        s1 = self.addSwitch( 's1' )
+        s2 = self.addSwitch( 's2' )
+        s3 = self.addSwitch( 's3' )
+        s4 = self.addSwitch( 's4' )
+        s5 = self.addSwitch( 's5' )
+        s6 = self.addSwitch( 's6' )
+        s7 = self.addSwitch( 's7' )
+        s8 = self.addSwitch( 's8' )
+        s9 = self.addSwitch( 's9' )
+        s10 = self.addSwitch( 's10' )
 
         # Add links
-        self.addLink( h1, s1 )
-        self.addLink( h2, s2 )
-        self.addLink( h3, s3 )
-        self.addLink( h4, s4 )
-        self.addLink( h5, s5 )
-        self.addLink( h6, s6 )
-        self.addLink( h7, s7 )
-        self.addLink( h8, s8 )
-        self.addLink( h9, s9 )
-        self.addLink( h10, s10 )
-        self.addLink( h11, s10 )
-        self.addLink( h12, s10 )
+        self.addLink( h11, s1 )
+        self.addLink( h12, s2 )
+        self.addLink( h13, s3 )
+        self.addLink( h14, s4 )
+        self.addLink( h15, s5 )
+        self.addLink( h16, s6 )
+        self.addLink( h17, s7 )
+        self.addLink( h18, s8 )
+        self.addLink( h19, s9 )
+        self.addLink( h20, s10 )
+        self.addLink( h21, s10 )
+        self.addLink( h22, s10 )
         self.addLink( s1, s2 )
         self.addLink( s1, s3 )
         self.addLink( s1, s6 )
